0920dfb1d6288b4f00015605352fa2c72e6a61a7
Nikita Frolkin <frolkinnikita94@gmail.com>
2017-01-12 20:08:06 +0300

    Add tests to parser

--- t/tests.lisp
+++ t/tests.lisp
@@ -1,6 +1,19 @@
-(in-package :common-lisp-user)
+(in-package #:common-lisp-user)
 
-(defpackage :web-cc.test
-  (:use :common-lisp
-        :fiveam))
-(in-package :web-cc.test)
+(defpackage #:web-cc.test
+  (:use #:common-lisp
+        #:fiveam))
+(in-package #:web-cc.test)
+
+(defparameter *test-numbers* '(("3.14" . 3.14)
+                               ("10." . 10.)
+                               (".001" . .001)
+                               ("1e10" . 1e10)
+                               ("3.14e-10" . 3.14e-10)
+                               ("0e0" . 0e0)))
+
+(test test-parse-numbers
+  (dolist (cell *test-numbers*)
+    (let ((expected (cdr cell))
+          (actual (web-cc:parse (car cell) :number)))
+      (is (= expected actual)))))
--- web-cc.asd
+++ web-cc.asd
@@ -5,3 +5,7 @@
   :serial t
   :components ((:file "packages")
                (:file "parser")))
+
+(defmethod asdf:perform ((o asdf:test-op) (c (eql (asdf:find-system :web-cc))))
+  (asdf:load-system :web-cc-test)
+  (uiop:symbol-call :fiveam :run-all-tests))
