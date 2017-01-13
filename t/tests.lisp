f504fcb103aba57c56a6132074864b1320780678
Nikita Frolkin <frolkinnikita94@gmail.com>
2017-01-14 00:41:06 +0300

    Write tests for constant grammar rule

--- t/tests.lisp
+++ t/tests.lisp
@@ -17,9 +17,30 @@
                                                            :max (char-code end-char)))))
     (funcall (gen-string :elements char-generator))))
 
+(defun random-float (bound)
+  (funcall (gen-float :bound bound)))
+
 
 (test test-parse-numbers
   (dolist (cell *test-numbers*)
     (let ((expected (cdr cell))
           (actual (web-cc:parse (car cell) :number)))
       (is (= expected actual)))))
+
+(test test-parse-constant
+  (let ((name (random-string #\A #\Z))
+        (value (random-float 10)))
+    (web-cc:def-constant name value)
+    (is (= value (web-cc:parse (string name) :number)))))
+
+(test test-signal-undefined-constant
+  (signals web-cc:undefined-constant-error
+    (web-cc:parse (random-string #\A #\Z))))
+
+(test test-signal-incorrect-constant-value-in-definition
+  (signals type-error
+    (web-cc:def-constant (random-string #\A #\Z) nil)))
+
+(test test-signal-incorrect-constant-name-in-expression
+  (signals esrap:esrap-parse-error
+    (web-cc:parse (random-string #\a #\z))))
