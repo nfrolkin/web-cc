2cc43c7cd7f23a6bd4b7bb1a8c98845ba6a8e7cb
Nikita Frolkin <frolkinnikita94@gmail.com>
2017-01-19 19:42:05 +0300

    Add error display

--- src/web.lisp
+++ src/web.lisp
@@ -10,4 +10,25 @@
   (render-template "index.html.clt"))
 
 (defun calculation-handler/post (expression)
-  (declare (ignore expression)))
+  (let ((context (create-context)))
+    (handler-case (compute expression)
+      (undefined-constant-error (e)
+        (setf (getf context :error-type) :undefined-constant
+              (getf context :error-message) (undefined-error-identifier e)))
+      (undefined-function-error (e)
+        (setf (getf context :error-type) :undefined-function
+              (getf context :error-message) (undefined-error-identifier e)))
+      (mismatch-argument-error (e)
+        (setf (getf context :error-type) :mismatch
+              (getf context :error-message) (list (mismatch-argument-error-function-name e)
+                                                  (mismatch-argument-error-args-expected e)
+                                                  (mismatch-argument-error-args-provided e))))
+      (parser-error (e)
+        (setf (getf context :error-type) :parse
+              (getf context :error-message) (parser-error-position e)))
+      (error ()
+        (setf (getf context :error-type) :calculation))
+      (:no-error (answer)
+        (setf (getf context :answer) answer)))
+    (setf (getf context :expression) expression)
+    (render-template "index.html.clt" context)))
