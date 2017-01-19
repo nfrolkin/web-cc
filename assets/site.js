8e92e95292286777f68c0dc4b4e6428cc3924b11
Nikita Frolkin <frolkinnikita94@gmail.com>
2017-01-19 23:08:39 +0300

    Add JavaScript

---
+++ assets/site.js
@@ -0,0 +1,14 @@
+function openModal(id) {
+  var modal = document.getElementById(id);
+  modal.style.display = 'block';
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      closeModal(id);
+    }
+  };
+}
+
+function closeModal(id) {
+  var modal = document.getElementById(id);
+  modal.style.display = 'none';
+}
